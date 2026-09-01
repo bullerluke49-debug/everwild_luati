65c0acf2fc6a7d02d8ed1db58b440127ce718e22
bullerluke49_debug <bullerluke49@gmail.com>
2026-09-01 06:17:16 -0700

    Debug crafting system

--- mods/crafting/crafting_item_flint_dagger.lua
+++ mods/crafting/crafting_item_flint_dagger.lua
@@ -1,5 +1,5 @@
 local lib_path = core.get_modpath("everwild_library")
 dofile(lib_path .. "/two_by_two_crafting.lua")
-two_by_two("everwild_items:flint_dagger","everwild_items:flint","everwild_items:stick","everwild_items:stick","everwild_items:stick")
+two_by_two("everwild_items:flint_dagger","everwild_items:flint","","","everwild_items:stick")
 
 
